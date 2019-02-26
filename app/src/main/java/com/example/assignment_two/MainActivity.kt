6988876b54ba491c294875538ba31e818aef9861
Chris Johnson <43618289+Cjohnson187@users.noreply.github.com>
2019-02-26 13:19:07 -0700

    working but still want to use spinner

--- app/src/main/java/com/example/assignment_two/MainActivity.kt
+++ app/src/main/java/com/example/assignment_two/MainActivity.kt
@@ -29,6 +29,18 @@ class MainActivity : AppCompatActivity() {
     var value3 = 0 // variable to set surface view color  ( Bl:ue )
 
     var save1 = arrayOf("new1", "0", "0", "0") // array variable for save button values
+    var save2 = arrayOf("new1", "0", "0", "0") // array variable for save button values
+    var save3 = arrayOf("new1", "0", "0", "0") // array variable for save button values
+
+
+    var bool1 = false
+    var bool2 = false
+    var bool3 = false
+
+    //val slot1 = button.findViewById<Button>(R.id.button1)
+    //val slot2 = button.findViewById<Button>(R.id.button)
+   // val slot3 = button.findViewById<Button>(R.id.button3)
+
 
 
     // initialize app
@@ -48,6 +60,11 @@ class MainActivity : AppCompatActivity() {
         val seekBar1 = this.findViewById<SeekBar>(R.id.seekBar1) // seek bar var
         val seekBar2 = this.findViewById<SeekBar>(R.id.seekBar2) // seek bar var
         val seekBar3 = this.findViewById<SeekBar>(R.id.seekBar3) // seek bar var
+
+        var slot1 = button.findViewById<Button>(R.id.button1)
+        var slot2 = button.findViewById<Button>(R.id.button)
+        var slot3 = button.findViewById<Button>(R.id.button3)
+
 
         // ***********************************************************************
 
@@ -122,27 +139,90 @@ class MainActivity : AppCompatActivity() {
             input.visibility = View.VISIBLE
             saveC.visibility = View.VISIBLE
 
+            var slot1 = this.findViewById<Button>(R.id.button1)
+            var slot2 = this.findViewById<Button>(R.id.button)
+            var slot3 = this.findViewById<Button>(R.id.button3)
+
+
+
+
             saveC.setOnClickListener {
 
-                var saveS = button.findViewById<Button>(R.id.button)
+                if (bool1 == false) {
+
+                    // saveC.setOnClickListener {
+
+                    //var slot1 = button.findViewById<Button>(R.id.button1)
 
 
-                save1[0] = textField.toString()
-                //val saveS = Toast.makeText(applicationContext, "$save1[0]" + " saved", Toast.LENGTH_LONG)
-                //saveS.show()
+                    save1[0] = textField.toString()
+                    //val saveS = Toast.makeText(applicationContext, "$save1[0]" + " saved", Toast.LENGTH_LONG)
+                    //saveS.show()
 
-                // save each value to array slot for rgb format
-                //save1[0] = colorName
-                save1[1] = value1.toString()
-                save1[2] = value2.toString()
-                save1[3] = value3.toString()
+                    // save each value to array slot for rgb format
+                    //save1[0] = colorName
+                    save1[1] = value1.toString()
+                    save1[2] = value2.toString()
+                    save1[3] = value3.toString()
 
-                input.visibility = View.INVISIBLE
-                saveC.visibility = View.INVISIBLE
-                //spin.visibility = View.VISIBLE
-                saveS.setText(save1[0])
+                    input.visibility = View.INVISIBLE
+                    saveC.visibility = View.INVISIBLE
+                    //spin.visibility = View.VISIBLE
+                    slot1.setText(save1[0])
+                    bool1 = true
+
+                } else if (bool2 == false) {
+
+                    // saveC.setOnClickListener {
+
+                    //var slot2 = button.findViewById<Button>(R.id.button)
+
+
+                    save2[0] = textField.toString()
+                    //val saveS = Toast.makeText(applicationContext, "$save1[0]" + " saved", Toast.LENGTH_LONG)
+                    //saveS.show()
+
+                    // save each value to array slot for rgb format
+                    //save1[0] = colorName
+                    save2[1] = value1.toString()
+                    save2[2] = value2.toString()
+                    save2[3] = value3.toString()
+
+                    input.visibility = View.INVISIBLE
+                    saveC.visibility = View.INVISIBLE
+                    //spin.visibility = View.VISIBLE
+                    slot2.setText(save2[0])
+                    bool2 = true
+
+                } else {
+
+                    // saveC.setOnClickListener {
+
+                    //var slot3 = button.findViewById<Button>(R.id.button3)
+
+
+                    save3[0] = textField.toString()
+                    //val saveS = Toast.makeText(applicationContext, "$save1[0]" + " saved", Toast.LENGTH_LONG)
+                    //saveS.show()
+
+                    // save each value to array slot for rgb format
+                    //save1[0] = colorName
+                    save3[1] = value1.toString()
+                    save3[2] = value2.toString()
+                    save3[3] = value3.toString()
+
+                    input.visibility = View.INVISIBLE
+                    saveC.visibility = View.INVISIBLE
+                    //spin.visibility = View.VISIBLE
+                    slot3.setText(save3[ 0])
+                    bool3 = true
+
+                }
 
             }
+
+
+
 
             true
         }
@@ -151,40 +231,220 @@ class MainActivity : AppCompatActivity() {
         // load button
         R.id.load -> {
 
-            var saveS = button.findViewById<Button>(R.id.button)
+            var slot1 = this.findViewById<Button>(R.id.button1)
+            var slot2 = this.findViewById<Button>(R.id.button)
+            var slot3 = this.findViewById<Button>(R.id.button3)
 
 
-            saveS.visibility = View.VISIBLE
+            if ((bool1 == false) && (bool3 == false) ) {
 
-            saveS.setOnClickListener {
-                val surface1 = this.surfaceView // create surface variable
+                slot1.visibility = View.VISIBLE
 
-                val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
-                val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
-                val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+                slot1.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
 
-                value1 = save1[1].toInt()
-                value2 = save1[2].toInt()
-                value3 = save1[3].toInt()
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
 
-                seekBar1.progress = value1 // set variable to seek bar value
-                seekBar2.progress = value2 // set variable to seek bar value
-                seekBar3.progress = value3 // set variable to seek bar value
+                    value1 = save1[1].toInt()
+                    value2 = save1[2].toInt()
+                    value3 = save1[3].toInt()
 
-                text1.text = "$value1"
-                text2.text = "$value2"
-                text3.text = "$value3"
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
 
-                text1.setTextColor(rgb(value1, 0, 0))
-                text2.setTextColor(rgb(0, value2, 0))
-                text3.setTextColor(rgb(0, 0, value3))
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
 
-                surface1.setBackgroundColor(rgb(save1[1].toInt(),save1[2].toInt(),save1[3].toInt()))
+                    text1.setTextColor(rgb(value1, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value3))
 
-                saveS.visibility = View.INVISIBLE
+                    surface1.setBackgroundColor(rgb(save1[1].toInt(),save1[2].toInt(),save1[3].toInt()))
+
+                    slot1.visibility = View.INVISIBLE
+
+                }
 
 
             }
+            else if ((bool2 == true) && (bool3 == false)) {
+                slot1.visibility = View.VISIBLE
+                slot2.visibility = View.VISIBLE
+
+                slot1.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
+
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+
+                    value1 = save1[1].toInt()
+                    value2 = save1[2].toInt()
+                    value3 = save1[3].toInt()
+
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
+
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
+
+                    text1.setTextColor(rgb(value1, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value3))
+
+                    surface1.setBackgroundColor(rgb(save1[1].toInt(),save1[2].toInt(),save1[3].toInt()))
+
+                    slot1.visibility = View.INVISIBLE
+                    slot2.visibility = View.INVISIBLE
+
+
+                }
+
+                slot2.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
+
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+
+                    value1 = save2[1].toInt()
+                    value2 = save2[2].toInt()
+                    value3 = save2[3].toInt()
+
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
+
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
+
+                    text1.setTextColor(rgb(value1, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value3))
+
+                    surface1.setBackgroundColor(rgb(save2[1].toInt(),save2[2].toInt(),save2[3].toInt()))
+
+                    slot2.visibility = View.INVISIBLE
+                    slot1.visibility = View.INVISIBLE
+
+                }
+
+
+
+            }
+
+            else {
+                slot1.visibility = View.VISIBLE
+                slot2.visibility = View.VISIBLE
+                slot3.visibility = View.VISIBLE
+
+                slot1.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
+
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+
+                    value1 = save1[1].toInt()
+                    value2 = save1[2].toInt()
+                    value3 = save1[3].toInt()
+
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
+
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
+
+                    text1.setTextColor(rgb(value1, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value3))
+
+                    surface1.setBackgroundColor(rgb(save1[1].toInt(),save1[2].toInt(),save1[3].toInt()))
+
+                    slot1.visibility = View.INVISIBLE
+                    slot2.visibility = View.INVISIBLE
+                    slot3.visibility = View.INVISIBLE
+
+
+                }
+                slot2.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
+
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+
+                    value1 = save2[1].toInt()
+                    value2 = save2[2].toInt()
+                    value3 = save2[3].toInt()
+
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
+
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
+
+                    text1.setTextColor(rgb(value2, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value2))
+
+                    surface1.setBackgroundColor(rgb(save2[1].toInt(),save2[2].toInt(),save2[3].toInt()))
+
+                    slot1.visibility = View.INVISIBLE
+                    slot2.visibility = View.INVISIBLE
+                    slot3.visibility = View.INVISIBLE
+
+
+
+                }
+                slot3.setOnClickListener {
+                    val surface1 = this.surfaceView // create surface variable
+
+                    val text1 = this.findViewById<TextView>(R.id.textB1) // text field to show seek values
+                    val text2 = this.findViewById<TextView>(R.id.textB2) // text field to show seek values
+                    val text3 = this.findViewById<TextView>(R.id.textB3) // text field to show seek values
+
+                    value1 = save3[1].toInt()
+                    value2 = save3[2].toInt()
+                    value3 = save3[3].toInt()
+
+                    seekBar1.progress = value1 // set variable to seek bar value
+                    seekBar2.progress = value2 // set variable to seek bar value
+                    seekBar3.progress = value3 // set variable to seek bar value
+
+                    text1.text = "$value1"
+                    text2.text = "$value2"
+                    text3.text = "$value3"
+
+                    text1.setTextColor(rgb(value1, 0, 0))
+                    text2.setTextColor(rgb(0, value2, 0))
+                    text3.setTextColor(rgb(0, 0, value3))
+
+                    surface1.setBackgroundColor(rgb(save3[1].toInt(),save3[2].toInt(),save3[3].toInt()))
+
+                    slot1.visibility = View.INVISIBLE
+                    slot2.visibility = View.INVISIBLE
+                    slot3.visibility = View.INVISIBLE
+
+
+                }
+
+
+
+            }
+
+
 
             true
         }
